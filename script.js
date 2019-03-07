88d9e969dedd30c5a0a459bf4a939b6608a8833d
montesantof <47189197+montesantof@users.noreply.github.com>
2019-03-07 12:22:05 -0500

    progress bar kinda works

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 $(document).ready(function() 
 {
-  var percentage = 10;
-  var s = document.getElementById("service").value;
+  var s = document.forms["inputs"]["service"].value;
+  var f = document.forms["inputs"]["food"].value;
+  var a = document.forms["inputs"]["atmos"].value;
   $("[name='submit']").click(function(e) 
   {
     e.preventDefault();
+    if(s=document.forms["inputs"]["service"].value){}
+    if(f=document.forms["inputs"]["food"].value){}
+    if(a=document.forms["inputs"]["atmos"].value){}
+
     $(".progress-bar-info").css("width", s + "%");
-    $(".progress-bar-success").css("width", s + "%");
-    $(".progress-bar-warning").css("width", percentage + "%");
+    $(".progress-bar-success").css("width", f + "%");
+    $(".progress-bar-warning").css("width", a + "%");
     
-    console.log(parseInt(s));
+    console.log(s);
     console.log(typeof s);
   });
 });
